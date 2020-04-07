9fc47f7d8c9798b22b66e4548b5a656de8108d41
BasCalder5 <62661069+BasCalder5@users.noreply.github.com>
2020-04-07 15:35:13 +0200

    SpaceInvaders Engine test

---
+++ SpaceInvaders/Game.cpp
@@ -0,0 +1,54 @@
+#include "Game.h"
+
+Game::Game() {}
+
+Game::~Game() {}
+
+void Game::init(const char *title, int xpos, int ypos, int width, int height) {
+    int flags = 0;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) //init succesfull if == 0
+    {
+        std::cout << "SDL init succesful..." << std::endl;
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+        if (window)
+            std::cout << "Window created succesfully..." << std::endl;
+        renderer = SDL_CreateRenderer(window, -1, flags);
+        if (renderer)
+        {
+            SDL_SetRenderDrawColor(renderer, 255,0,255, 255);
+            std::cout << "Renderer created succesfully..." << std::endl;
+        }
+
+        isRendering = true;
+    } else
+        isRendering = false;
+}
+
+void Game::handle() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+        case SDL_QUIT:
+            isRendering = false;
+            break;
+        default:
+            break;
+    }
+}
+
+void Game::update() {
+}
+
+void Game::render() {
+    SDL_RenderClear(renderer);
+    // add stuff to render
+    SDL_RenderPresent(renderer);
+}
+
+void Game::destroy() {
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    std::cout << "Window destroyed!" << std::endl;
+}
--- SpaceInvaders/main.cpp
+++ SpaceInvaders/main.cpp
@@ -1,11 +1,20 @@
+#include "Game.h"
+#include "SDL2/SDL.h"
 
-int main(int argc, const char * argv[]) {
+Game *game = nullptr;
 
-    while() // Game is running
+int main(int argc, char ** argv) {
+
+    game = new Game;
+
+    game->init("SpaceInvaders Engine test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720, 480);
+
+    while(game->rendering()) // Game is running
     {
-        // Handle input
-        // Update objects
-        // Render changes
+        game->handle(); // Handle input
+        game->update(); // Update objects
+        game->render(); // Render changes
     }
+    game->destroy();
     return 0;
 }
