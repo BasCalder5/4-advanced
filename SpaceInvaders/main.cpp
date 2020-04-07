3c6d42196575eebc452a5ab16f7dc7961a3d5317
BasCalder5 <62661069+BasCalder5@users.noreply.github.com>
2020-04-07 19:12:59 +0200

    added menubg asset and changed resolution + addes scale

--- SpaceInvaders/main.cpp
+++ SpaceInvaders/main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char ** argv) {
 
     const int FPS = 60;
     const int fDelay = 1000/FPS;
+    const int SCALE = 2;
+    const int HEIGHT = 360;
+    const int WIDTH = 540;
 
     Uint32 fStart;
     int fTime;
@@ -14,7 +17,7 @@ int main(int argc, char ** argv) {
 
     game = new Game;
 
-    game->init("SpaceInvaders Engine test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720, 480);
+    game->init("SpaceInvaders Engine test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH * SCALE , HEIGHT * SCALE);
 
     while(game->rendering()) // Game is running
     {
